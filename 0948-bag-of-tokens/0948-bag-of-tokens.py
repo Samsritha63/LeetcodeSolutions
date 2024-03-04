71a572184062f4031fff6c0d6a4df9ef7ebfee7e
Dharani Samsritha <84120086+Samsritha63@users.noreply.github.com>
2024-03-04 23:25:43 +0530

    Time: 54 ms (53.66%), Space: 16.7 MB (64.63%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.py
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -1,9 +1,13 @@
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        #create a queue which can be accessed from front and back
         q=deque(sorted(tokens))
+        # intialize score and max_score 
         score=0
         max_score=0
+        #while the q is not empty
         while q:
+            # 2 options to play token
             if power>=q[0]:
                 points=q.popleft()
                 power-=points
@@ -13,6 +17,7 @@ class Solution:
                 points=q.pop()
                 power+=points
                 score-=1
+            #if the 2 tokens doesnt have bandwidth
             else:
                 break
         return max_score
