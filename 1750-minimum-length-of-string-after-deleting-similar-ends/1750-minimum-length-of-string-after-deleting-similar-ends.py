09a4e85f9a6957c8feda95c56eb076530c7d2ed4
Dharani Samsritha <84120086+Samsritha63@users.noreply.github.com>
2024-03-06 00:20:40 +0530

    Time: 61 ms (86.73%), Space: 17.3 MB (75.83%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -1,15 +1,22 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
+        # create 2 pointers
         left=0
         right=len(s)-1
+        # only enter if the ends are same
         while (left<right) and s[left]==s[right]:
+            # pick the common character
             ch=s[left]
+            # get prefix with same characters
             while (left<=right) and s[left] == ch:
                 left+=1
+            # get suffix with same characters
             while (right>=left) and s[right] == ch:
                 right-=1
+        # make sure that left and right doesnt intersect, if they do, nothing works
         if left > right:
             return 0
+        # return the result
         else:
             return right-left+1
             
