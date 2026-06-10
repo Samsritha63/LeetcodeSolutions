6c1bd60711b8bcce868a0f63e282dd608c3c0379
Dharani Samsritha <84120086+Samsritha63@users.noreply.github.com>
2026-06-10 08:07:15 +0530

    Time: 10 ms (37.57%), Space: 19.8 MB (38.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,9 +4,4 @@ class Solution:
         for i in s:
             if i.isalnum():
                 res+=i.lower()
-        mid = len(res) // 2
-        if (len(res)%2) == 0 and res[:mid] == res[mid:][::-1]:
-            return True
-        elif (len(res)%2) !=0 and res[:mid] == res[mid+1:][::-1]:
-            return True
-        return False
+        return res == res[::-1]
